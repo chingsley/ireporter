52951fa408548e3ac400099190862b40e5dcd015
chingsley <eneja.kc@gmail.com>
2019-01-19 22:54:11 +0100

    implement the savechanges() function to handle all patch operations simultaneously

--- challenge-4/UI/js/editrecord.js
+++ challenge-4/UI/js/editrecord.js
@@ -2,6 +2,7 @@
 const address = document.getElementById('address');
 const addressContainer = document.getElementById('rep-field-for-address');
 const btnChangeLocation = document.getElementById('btn-change-location');
+const btnGetCurrentLocation = document.getElementById('btn-get-current-location');
 const btnSaveComment = document.getElementById('btn-save-comment');
 const btnSaveChanges = document.getElementById('btn-save-changes');
 const btnSaveLocation = document.getElementById('btn-save-location');
@@ -24,18 +25,19 @@ const vidFileInputContainer = document.querySelector('.vid-file-input-container'
 const vidUploadInfo = document.querySelector('.vid-upload-info');
 
 let allowedUploadCount = 0;
-let x = 0;
-let y = 0;
+let geocoder;
+let infowindow;
+let map;
 let msg = '';
 let recordAddress = '';
+let resultArr = [];
+let x = 0;
+let y = 0;
 
 coords.title = 'readOnly';
 spanRecordId.innerText = recordId;
 spanRecordType.innerText = recordType.toString().toUpperCase();
 
-// console.log(imgFileInput);
-// console.log(imgUploadInfo);
-// console.log(imgContainer);
 
 {// FETCH THE SPECIFIC RECORD OF THE GIVEN recordId
     const uri = `${root}/${recordType}s/${recordId}`;
@@ -63,51 +65,17 @@ spanRecordType.innerText = recordType.toString().toUpperCase();
 }
 
 {// HANDLING GEOLOCATION (NOTE: THE CALL FOR THE 'PATCH' OPERATION HAPPENS WITHING THE 'geocodeAddress' FUNCTION
-    // let btnGetCurrentLocation = document.getElementById('btn-get-current-location');
     function initMap() {
-        let map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
             zoom: 8,
             center: { lat: 6.465422, lng: 3.406448 }
         });
 
-        let geocoder = new google.maps.Geocoder();
-        let infowindow = new google.maps.InfoWindow();
-
-        window.addEventListener('load',() => {
-                geocodeLatLng(geocoder, map, infowindow);
-        }, false);
-        
-        btnSaveLocation.addEventListener('click', (event) => {
-            event.preventDefault();
-            geocodeAddress(geocoder, map, infowindow);
-        });
-
-        // // Use this to allow the user generate the coordinates of the current location
-        // btnGetCurrentLocation.addEventListener('click', function (event) {
-        //     event.preventDefault();
-        //     // getLocation();
-        //     // let outcome = getLocation();
-        //     let p1 = new Promise((resolve, reject) => {
-        //        let outcome =  getLocation();
-        //         if(outcome === true) {
-        //             resolve(outcome);
-        //         } else {
-        //             reject("Geolocation is not supported by this browser.");
-        //         }
-        //     });
-        //     p1.then(result => {
-        //         console.log(result);
-        //         console.log(coords.value);
-        //         geocodeLatLng(geocoder, map, infowindow);
-        //     }).catch(err => {
-        //         showDialogMsg(0, 'Error', err, 'center');
-        //     });
-        // });
-
+        geocoder = new google.maps.Geocoder();
+        infowindow = new google.maps.InfoWindow();
     }
 
     function geocodeAddress(geocoder, resultsMap, infowindow) {
-        // let address = document.getElementById('address').value;
         let p1 = new Promise((resolve, reject) => {
             geocoder.geocode({ 'address': address.value }, function (results, status) {
                 if (status === 'OK') {
@@ -123,8 +91,6 @@ spanRecordType.innerText = recordType.toString().toUpperCase();
                     infowindow.open(map, marker);
                     resolve(true);
                 } else {
-                    // isValidCoords = false;
-                    // alert('The address you entered is unknown: ' + status);
                     if (status === 'ERROR') {
                         msg = `Theres was a problem with geolocation. <br>Please check your internet connection: ` + status;
                         // reject(`Theres was a problem with geolocation. <br>Please check your internet connection: ` + status);
@@ -138,24 +104,7 @@ spanRecordType.innerText = recordType.toString().toUpperCase();
             });
         });
 
-        p1.then((result) => {
-            console.log('result', result); // true (because: resolve(true))
-            const uri = `${root}/${recordType}s/${recordId}/location`; // notice to the addition of 's' to make interventions and red-flags
-
-            const formdata = new FormData();
-            formdata.append('location', coords.value);
-
-            const myHeaders = new Headers();
-            // myHeaders.append('Content-Type', 'application/json'); // This results in error during fetch
-            myHeaders.append('x-auth-token', token);
-
-            const options = { method: 'PATCH', mode: 'cors', headers: myHeaders, body: formdata, };
-            const req = new Request(uri, options);
-            // patchLocation(req);
-            setTimeout(() => { patchLocation(req); }, 1000);
-        }).catch(err => {
-            showDialogMsg(0, err.errType, err.message, 'center');
-        });
+        return p1;
     }
 
     function geocodeLatLng(geocoder, resultsMap, infowindow) {
@@ -192,75 +141,12 @@ spanRecordType.innerText = recordType.toString().toUpperCase();
         coords.value = `${position.coords.latitude}, ${position.coords.longitude}`;
     }
 
-    // // Use this to allow the user generate the coordinates of the current location
-    // btnGetCurrentLocation.addEventListener('click', function (event) {
-    //     event.preventDefault();
-    //     getLocation();
-    //     geocodeLatLng(geocoder, map, infowindow);
-    // });
 }
 
-{// ADD EVENT LISTENER TO THE ADDRESS INPUT WITH SOME ANIMATIONS
-
-    // show the 'save changes' button when the user changes the address
-    address.addEventListener('input', () => {
-        address.style.backgroundColor = 'transparent';
-        btnSaveLocation.style.display = 'block';
-        btnSaveLocation.style.animation = 'moveInLeft 1s ease';
-    });
-    address.addEventListener('blur', () =>{
-        address.style.backgroundColor = 'rgba(128, 128, 128, .5)';
-    })
-}
-
-{// SET UP FOR PATCH COMMENT
-    comment.addEventListener('input', () => {
-        btnSaveComment.style.display = 'block';
-        btnSaveComment.style.animation = 'moveInLeft 1s ease';
-        comment.style.backgroundColor = 'transparent';
-    });
-    comment.addEventListener('blur', () => {
-        comment.style.backgroundColor = 'rgba(128, 128, 128, .5)';
-    });
-
-    btnSaveComment.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        const uri = `${root}/${recordType}s/${recordId}/comment`; // notice to the addition of 's' to make interventions and red-flags
-        const formdata = new FormData();
-        formdata.append('comment', comment.value);
-        const myHeaders = new Headers();
-        myHeaders.append('x-auth-token', token);
-        const options = { method: 'PATCH', mode: 'cors', headers: myHeaders, body: formdata, };
-        const req = new Request(uri, options);
-        patchComment(req);
-    });
-}
 
 
 const addImg = () => {
-    const formdata = new FormData();
-    const myHeaders = new Headers();
-    const uri = `${root}/${recordType}s/${recordId}/addImage`;
-    const images = imgFileInput.files;
-    
-
-    myHeaders.append('x-auth-token', token);
-    if(images.length > 0 ) {
-        for(let i = 0; i < images.length; i += 1) {
-            formdata.append('images', images[i], `${recordId}_${images[i].name}`);
-        }
-    }
-
-    const options = { 
-         method: 'PATCH',
-         mode: 'cors', 
-         headers: myHeaders, 
-         body: formdata, 
-    };
-
-    const req = new Request(uri, options);
-
+    const req = getRequestObj('addImage');
     fetch(req)
     .then(response => {
         return response.json();
@@ -280,29 +166,9 @@ const addImg = () => {
     });
 
 };
+
 const addVideo = () => {
-    const formdata = new FormData();
-    const myHeaders = new Headers();
-    const uri = `${root}/${recordType}s/${recordId}/addVideo`;
-    const videos = vidFileInput.files;
-    
-
-    myHeaders.append('x-auth-token', token);
-    if(videos.length > 0 ) {
-        for(let i = 0; i < videos.length; i += 1) {
-            formdata.append('videos', videos[i], `${recordId}_${videos[i].name}`);
-        }
-    }
-
-    const options = { 
-         method: 'PATCH',
-         mode: 'cors', 
-         headers: myHeaders, 
-         body: formdata, 
-    };
-
-    const req = new Request(uri, options);
-
+    const req = getRequestObj('addVideo');
     fetch(req)
     .then(response => {
         return response.json();
@@ -323,10 +189,6 @@ const addVideo = () => {
 
 };
 
-const getAllowedUploadCount = (mediaArr) => {
-    return MEDIA_MAX_COUNT - mediaArr.length;
-}
-
 const displayContents = (response) => {
     coords.value = response.data[0].location;
     comment.value = response.data[0].comment;
@@ -336,34 +198,10 @@ const displayContents = (response) => {
 
     displayMedia(images, 'image');
     displayMedia(videos, 'video');
-
-    // console.log(videos);
-    // let info = '';
-    // if(images.length === MEDIA_MAX_COUNT) {
-    //     info = `you have reached the maximum upload of ${MEDIA_MAX_COUNT} images`;
-    //     imgFileInputContainer.style.display = 'none'; // hide the img file input element
-    // } else if (images.length < MEDIA_MAX_COUNT) {
-    //     allowedUploadCount = MEDIA_MAX_COUNT - images.length;
-    //     info = `you have uploaded ${images.length}/${MEDIA_MAX_COUNT} images`;
-    // } else {
-    //     info = `you have exceeded the maximum upload count of ${MEDIA_MAX_COUNT}`;
-    //     imgFileInputContainer.style.display = 'none';
-    // }
-
-    // imgUploadInfo.innerHTML = info;
-    // images.forEach(image => {
-    //     const imgDiv = document.createElement('div');
-    //     imgDiv.classList.add('ic_image-div');
-    //     imgContainer.appendChild(imgDiv);
-    //     const img = document.createElement('img');
-    //     img.classList.add('record-img');
-    //     img.src = `${imgRoot}/${image}`;
-    //     imgDiv.appendChild(img);
-    // });
 };
 
 const displayMedia = (mediaArr, mediaType) => {
-    console.log(mediaType, mediaArr);
+    // console.log(mediaType, mediaArr);
     let info = '';
     let mediaContainer;
     let mediaFileInputDiv;
@@ -375,15 +213,15 @@ const displayMedia = (mediaArr, mediaType) => {
         mediaFileInputDiv = imgFileInputContainer;
         mediaUploadInfo = imgUploadInfo;
         mediaContainer = imgContainer;
-        console.log(imgContainer);
-        console.log(mediaContainer);
+        // console.log(imgContainer);
+        // console.log(mediaContainer);
     } else if (mediaType === 'video') {
         y = getAllowedUploadCount(mediaArr);
         mediaFileInputDiv = vidFileInputContainer;
         mediaUploadInfo = vidUploadInfo;
         mediaContainer = vidContainer;
-        console.log(vidContainer);
-        console.log(mediaContainer);
+        // console.log(vidContainer);
+        // console.log(mediaContainer);
     } else {
         throw new Error(`displayMedia() expects mediaType to be 'image' or 'video' `);
     }
@@ -403,13 +241,13 @@ const displayMedia = (mediaArr, mediaType) => {
         if (mediaType === 'image') {
             mediaDiv.classList.add('ic_image-div');
             media = document.createElement('img');
-            console.log(media);
+            // console.log(media);
             media.classList.add('record-img');
         } else {
             mediaDiv.classList.add('vc_video-div');
             media = document.createElement('video');
             media.controls = true;
-            console.log(media);
+            // console.log(media);
             media.classList.add('record-vid');
         }
         mediaContainer.appendChild(mediaDiv);
@@ -418,7 +256,56 @@ const displayMedia = (mediaArr, mediaType) => {
     });
 };
 
-const patchComment = (req) => {
+const getAllowedUploadCount = (mediaArr) => {
+    return MEDIA_MAX_COUNT - mediaArr.length;
+}
+
+const getRequestObj = (str) => {
+    const formdata = new FormData();
+    const myHeaders = new Headers();
+    const uri = `${root}/${recordType}s/${recordId}/${str}`;
+    const images = imgFileInput.files;
+    const videos = vidFileInput.files;
+
+    myHeaders.append('x-auth-token', token);
+
+    switch(str) {
+        case('location'):
+            formdata.append('location', coords.value);
+            break;
+        case('comment') : 
+            formdata.append('comment', comment.value);
+            break;
+        case('addImage') :
+            if (images.length > 0) {
+                for (let i = 0; i < images.length; i += 1) {
+                    formdata.append('images', images[i], `${recordId}_${images[i].name}`);
+                }
+            }
+            break;
+        case('addVideo') : 
+            if (videos.length > 0) {
+                for (let i = 0; i < videos.length; i += 1) {
+                    formdata.append('videos', videos[i], `${recordId}_${videos[i].name}`);
+                }
+            }
+            break;
+    }
+
+    const options = {
+        method: 'PATCH',
+        mode: 'cors',
+        headers: myHeaders,
+        body: formdata,
+    };
+
+    const req = new Request(uri, options);
+
+    return req;
+};
+
+const patchComment = () => {
+    const req = getRequestObj('comment');
     fetch(req)
         .then(response => {
             return response.json();
@@ -436,32 +323,136 @@ const patchComment = (req) => {
         });
 };
 
-const patchLocation = (req) => {
+const patchLocation = () => {
+    const req = getRequestObj('location');
     fetch(req)
-        .then(response => {
-            return response.json();
-        })
-        .then(response => {
-            if (response.status === 200) {
-                // sessionStorage.recordId = response.data[0].id;
-                msg = `<p>${response.data[0].message}</p>
-                       <p>Closest landmark: ${recordAddress}</p>`;
-                showDialogMsg(2, 'Saved', msg, 'center');
-            } else {
-                handleResponseError(response);
-            }
-        })
-        .catch(err => {
-            handleError(err);
-        });
+    .then(response => {
+        return response.json();
+    })
+    .then(response => {
+        if (response.status === 200) {
+            // sessionStorage.recordId = response.data[0].id;
+            msg = `<p>${response.data[0].message}</p>
+                    <p>Closest landmark: ${recordAddress}</p>`;
+            showDialogMsg(2, 'Saved', msg, 'center');
+        } else {
+            handleResponseError(response);
+        }
+    })
+    .catch(err => {
+        handleError(err);
+    });
 };
 
+let count;
+const saveChanges = () => {
+    count = 0;
+    resultArr = [];
+    msg = '';
+    const saveLocation = fetch(getRequestObj('location'));
+    const saveComment = fetch(getRequestObj('comment'));
+    const saveImages = fetch(getRequestObj('addImage'));
+    const saveVideos = fetch(getRequestObj('addVideo'));
+
+    promiseArr = [saveLocation, saveComment, saveImages, saveVideos];
+
+    Promise.all(promiseArr)
+    .then(responseArr => {
+        console.log(responseArr);
+        responseArr.forEach(response => {
+            process(response.json());
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        showDialogMsg(0, 'Error', err.message, 'center');
+    });
+};
+
+const process = (jsonResult) => {
+    jsonResult.then((responseObj) => {
+        console.log(responseObj);
+        resultArr.push(responseObj);
+        console.log(count, promiseArr.length -1);
+        if(count === promiseArr.length - 1) { // 3 fetch operations : 0 , 1, 2
+            console.log(resultArr);
+            let errorResponse = checkForErrors(resultArr);
+            if (errorResponse.length > 0 ) {
+                errorResponse.forEach(response => {
+                    msg += `${response.error}<br>`;
+                })
+            }
+            showDialogMsg(0, 'Error', msg, 'center');
+        }
+        count += 1;
+    });
+};
+
+const checkForErrors = (resultArr) => {
+    let errResults = resultArr.filter(result => {
+        if(!(result.status >= 200 && result.status < 400)) {
+            return result;
+        }
+    });
+    console.log(errResults);
+    return errResults;
+};
+
+
+
+address.addEventListener('input', () => {
+    address.style.backgroundColor = 'transparent';
+    btnSaveLocation.style.display = 'block';
+    btnSaveLocation.style.animation = 'moveInLeft 1s ease';
+});
+
+address.addEventListener('blur', () => {
+    address.style.backgroundColor = 'rgba(128, 128, 128, .5)';
+});
 
 btnSaveChanges.addEventListener('click', (event) => {
     event.preventDefault();
 
-    addImg();
-    // addVideo();
+    let p1 = geocodeAddress(geocoder, map, infowindow);
+
+    p1.then((result) => {
+        console.log('result', result); // true (because: resolve(true))
+
+        setTimeout(() => { saveChanges(); }, 1000);
+    }).catch(err => {
+        console.log(err);
+        showDialogMsg(0, err.errType, err.message, 'center');
+    });
+    // saveChanges();
+});
+
+btnSaveComment.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    patchComment();
+});
+
+btnSaveLocation.addEventListener('click', (event) => {
+    event.preventDefault();
+    let p1 = geocodeAddress(geocoder, map, infowindow);
+
+    p1.then((result) => {
+        // console.log('result', result); // true (because: resolve(true))
+
+        setTimeout(() => { patchLocation(); }, 1000);
+    }).catch(err => {
+        showDialogMsg(0, err.errType, err.message, 'center');
+    });
+});
+
+comment.addEventListener('blur', () => {
+    comment.style.backgroundColor = 'rgba(128, 128, 128, .5)';
+});
+
+comment.addEventListener('input', () => {
+    btnSaveComment.style.display = 'block';
+    btnSaveComment.style.animation = 'moveInLeft 1s ease';
+    comment.style.backgroundColor = 'transparent';
 });
 
 imgFileInput.addEventListener('change', () => {
@@ -484,7 +475,7 @@ imgFileInput.addEventListener('change', () => {
          
         imgFileInput.value = "";
     }
-    console.log(files);
+    // console.log(files);
 });
 
 vidFileInput.addEventListener('change', () => {
@@ -512,3 +503,40 @@ vidFileInput.addEventListener('change', () => {
         vidFileInput.value = "";
     }
 });
+
+window.addEventListener('load', () => {
+    geocodeLatLng(geocoder, map, infowindow);
+}, false);
+
+
+
+
+ // // Use this to allow the user generate the coordinates of the current location
+// btnGetCurrentLocation.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     // getLocation();
+//     // let outcome = getLocation();
+//     let p1 = new Promise((resolve, reject) => {
+//        let outcome =  getLocation();
+//         if(outcome === true) {
+//             resolve(outcome);
+//         } else {
+//             reject("Geolocation is not supported by this browser.");
+//         }
+//     });
+//     p1.then(result => {
+        // console.log(result);
+        // console.log(coords.value);
+//         geocodeLatLng(geocoder, map, infowindow);
+//     }).catch(err => {
+//         showDialogMsg(0, 'Error', err, 'center');
+//     });
+// });
+
+
+ // // Use this to allow the user generate the coordinates of the current location
+// btnGetCurrentLocation.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     getLocation();
+//     geocodeLatLng(geocoder, map, infowindow);
+// });
