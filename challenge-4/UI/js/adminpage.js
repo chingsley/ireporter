d5f0a02de79007ae813e030d62cbc139b0fa2850
chingsley <eneja.kc@gmail.com>
2019-01-21 15:58:54 +0100

    use DOM to populate the contents of the popup

--- challenge-4/UI/js/adminpage.js
+++ challenge-4/UI/js/adminpage.js
@@ -1,34 +1,107 @@
-console.log('sessionStorage.token from reporthistory.js = ', sessionStorage.token);
-const token = sessionStorage.token;
-const myHeaders = new Headers();
-myHeaders.append('Content-Type', 'application/json');
-// myHeaders.append('x-auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInVzZXJOYW1lIjoibm9sbHkiLCJ1c2VyRW1haWwiOiJjaGluZ3NsZXlAZ21haWwuY29tIiwidXNlclN0YXR1cyI6ImFkbWluIiwiaWF0IjoxNTQ1MjExMjU2fQ.UYFA9N9tufof9CrMPB9EwzL43TfE80R1la9aFT7G19E');
-myHeaders.append('x-auth-token', token);
 
-const options = {
-    method: 'GET',
-    headers: myHeaders,
+const createCell = (arrOfClassNames, textContent) => {
+    let td = document.createElement('td');
+    arrOfClassNames.forEach(className => {
+        td.classList.add(className);
+    });
+    td.textContent = textContent;
+    return td;
+}
+
+const getAllRecords = async () => {
+
+    const token = sessionStorage.token;
+
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('x-auth-token', token);
+
+    const options = { method: 'GET', headers: myHeaders, };
+
+    redflagsURL = `${root}/red-flags`;
+    interventionsURL = `${root}/interventions`;
+
+    try {
+        const redflagsResponse = await fetch(redflagsURL, options);
+        const redflags = await redflagsResponse.json();
+        const interventionsResponse = await fetch(interventionsURL, options)
+        const interventions = await interventionsResponse.json();
+  
+        return redflags.data.concat(interventions.data) ;
+
+    } catch(err) {
+        console.log(err);
+        handleError(err);
+    }
+    
 };
-redflagsURL = 'http://localhost:3000/api/v1/red-flags';
-interventinsURL = 'http://localhost:3000/api/v1/interventions';
-const reqRed = fetch(redflagsURL, options);
-const reqInt = fetch(interventinsURL, options);
 
-Promise.all([reqRed, reqInt])
-    .then((responseArr) => {
-        console.log('responseArr', responseArr);
-        responseArr.forEach(response => {
-            process(response.json()); // the .json() method returns a promise
+const popup = (record) => {
+    const defaultImg = (record.type === 'red-flag') ?
+        "../img/redflag-img.png" :
+        "../img/intervention-img.png";
+
+    let a = document.createElement('a');
+    a.textContent = '...';
+    a.classList.add('report-card-comments-and-media');
+    a.addEventListener('click', () => {
+        // display the popup with record comments and images
+        const popup = document.getElementById('popup');
+        console.log(popup);
+        const popupContent = document.getElementById('popup-content');
+        console.log(popupContent);
+        popup.style.opacity = 1;
+        popup.style.visibility = 'visible';
+        popupContent.style.opacity = 1;
+        popupContent.style.transform = 'translate(-50%, -50%) scale(1)';
+
+        // replace popup comments with record's comment
+        const popupText = document.getElementsByClassName('popup-text');
+        const popupImg = document.getElementsByClassName('popup-img');
+
+        popupText[0].textContent = record.comment;
+        popupImg[0].src = record.Images[0] ? `${imgRoot}/${record.Images[0]}` : defaultImg;
+        popupImg[1].src = record.Images[1] ? `${imgRoot}/${record.Images[1]}` : defaultImg;
+
+        // close popup
+        const popupClose = document.getElementById('popup-close');
+        popupClose.addEventListener('click', () => {
+            popup.style.opacity = 0;
+            popup.style.visibility = 'hidden';
+            popupContent.style.opacity = 0;
+            popupContent.style.transform = 'translate(-50%, -50%) scale(.25)';
         });
-    }).catch(err => {
-        console.log('error message: ', err);
-        // use DOM to construct a dialog box for error message
-        // most likely error here will be a network failure
     });
 
-const process = (promisedJson) => {
-    promisedJson.then((dataArr) => {
-        console.log('dataArr', dataArr);
-        // use DOM to render the contents of 'dataArr' on reporthistory.html page
-    });
+    return a;
 };
+
+const displayRecords = async () => {
+    records = await getAllRecords();
+    console.log(records);
+   
+    const table = document.getElementById('table-admin');
+
+    records.forEach(record => {
+        const tr = document.createElement('tr');
+        tr.classList.add('row');
+
+        const id = createCell(['cell', 'id'], record.id);
+        const dateCreated = JSON.stringify(record.createdOn).slice(1, 11);
+        const createdOn = createCell(['cell', 'createdon'], dateCreated);
+        const createdBy = createCell(['cell', 'createdby'], record.createdBy);
+        const type = createCell(['cell', 'type'], record.type);
+        const location = createCell(['cell', 'location'], record.location);
+        const commentAndMedia = createCell(['cell', 'comment-and-media']);
+        commentAndMedia.appendChild(popup(record));
+        
+        console.log(commentAndMedia);
+        document.body.appendChild(commentAndMedia);
+        tr.appendChild(commentAndMedia);
+        table.appendChild(tr);
+        // console.log(id, createdOn, createdBy);
+    });
+} 
+
+displayRecords();
+
